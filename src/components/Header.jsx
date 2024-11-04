870a071830d2b3b452decc235a8b9e5879dda76b
Åsa Johansson <asa.j.1989@gmail.com>
2024-11-04 15:20:16 +0100

    grund för mobilmeny designad

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import SiliconLogo from '../images/silicon.svg'
 import SiliconLogoDark from '../images/silicon-dark.svg'
 import { NavLink, Link } from 'react-router-dom'
+import MobileMenu from './MobileMenu'
 
 const Header = () => {
   return (
@@ -23,10 +24,7 @@ const Header = () => {
                 <i className="fa-regular fa-user"></i>
                 <span>Sign in / up</span>
             </a>
-            <button className="btn-mobile-menu" aria-label="Menu">
-                <i id="mob-menu-light" className="fa-solid fa-bars"></i>
-                <i loading="lazy" id="mob-menu-dark" className="fa-solid fa-bars" style={{color: "#ededed"}}></i>       
-            </button>      
+            <MobileMenu />     
         </div>
     </header>
   )
---
+++ src/components/MobileMenu.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const MobileMenu = () => {
+    const [isActive, setIsActive] = useState(false)
+
+    const toggleActive = () => {
+        setIsActive(!isActive);
+    }
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+        toggleActive();
+    }
+
+    const closeDropDown = () => {
+        setIsOpen(false);
+    }
+
+  return (
+    <>
+        <div className="dropdown-container">
+            <button className={`btn-mobile-menu ${isActive ? 'menu-active' : ''}`} aria-label="Menu" onClick={handleClick}>
+                <i id="mob-menu-light" className="fa-solid fa-bars"></i>
+                <i loading="lazy" id="mob-menu-dark" className="fa-solid fa-bars" style={{color: "#ededed"}}></i>       
+            </button> 
+            {isOpen && (
+                <div className="dropdown-menu">
+                    <Link to="/" onClick={closeDropDown}><p>Home</p></Link>
+                    <Link to="/" onClick={closeDropDown}><p>Features</p></Link>
+                    <Link to="/contact" onClick={closeDropDown}><p>Contact</p></Link>
+                </div>
+            )}
+        </div>
+    </>
+  )
+}
+
+export default MobileMenu
