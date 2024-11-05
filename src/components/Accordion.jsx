6d08bbbc81f303388f71888896c42b539cc5a83c
Åsa Johansson <asa.j.1989@gmail.com>
2024-11-05 17:24:00 +0100

    accordion animation

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -4,125 +4,57 @@ const Accordion = () => {
 
     // https://win24-assignment.azurewebsites.     /swagger/v1/swagger.json
 
-    // const [users, setUsers] = useState([])
-
-    // const fetchUserData = () => {
-    //   fetch("https://jsonplaceholder.typicode.com/users")
-    //     .then(response => {
-    //       return response.json()
-    //     })
-    //     .then(data => {
-    //       setUsers(data)
-    //     })
-    // }
   
-    // useEffect(() => {
-    //   fetchUserData()
-    // }, [])
-  
-    const [isActive, setIsActive] = useState('')
-
-    const accordionData = {
-        title: 'Is any of my personal information stored in the App?',
-        content: 'Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.',
+    
+    const [isActive, setIsActive] = useState(null);
+    
+    const handleClick = (index) => {
+        setIsActive(isActive === index ? null : index);
     }
 
-    const {title, content} = accordionData;
+    
+    const accordionData = [
+        {
+            title: '111111Is any of my personal information stored in the App?',
+            content: 'Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.',
+        },
+        {
+            title: '222222Is any of my personal information stored in the App?',
+            content: 'Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.',
+        },
+        {
+            title: '3333333s any of my personal information stored in the App?',
+            content: 'Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.',
+        },
+        {
+            title: '444444 any of my personal information stored in the App?',
+            content: 'Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.',
+        },
+        {
+            title: '555555s any of my personal information stored in the App?',
+            content: 'Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.',
+        },
+        {
+            title: '666666s any of my personal information stored in the App?',
+            content: 'Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.',
+        }
+
+    ]
     
 
-        
-    const handleClick = (e) => {
-        e.preventDefault();
-        setIsActive(!isActive);
-    }
-
-
   return (
-    <>
-     {/* <div>
-      {users.length > 0 && (
-        <ul>
-          {users.map(user => (
-            <li key={user.id}>{user.name}</li>
-          ))}
-        </ul>
-      )}
-    </div> */}
-
-        {/* <div className="list-item">
-            <input type="radio" name="radio" id="radio1" />
+      <>
+      {accordionData.map((item, index) => (
+        <div className="list-item" key={index}>
+            <input type="radio" name="radio" id={index} onClick={() => handleClick(index)} />
             <div className="title">
-                <label htmlFor="radio1" className="question">Is any of my personal information stored in the App?</label>
-                <label htmlFor="radio1" className="btn btn-circle"><i className="fa-solid fa-chevron-down"></i></label>
+                <label htmlFor={index} className="question">{item.title}</label>
+                <label htmlFor={index} className={`btn btn-circle ${isActive === index ? 'btn-open' : ''}`}><i className="fa-solid fa-chevron-down"></i></label>
             </div>
-            <div className="content">Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.</div>
-         </div> */}
-        <div className="list-item">
-            <input type="radio" name="radio" id="radio1" onClick={handleClick} />
-            <div className="title">
-                <label htmlFor="radio1" className="question">{title}</label>
-                <label htmlFor="radio1" className={`btn btn-circle ${!isActive ? 'btn-open' : ''}`}><i className="fa-solid fa-chevron-down"></i></label>
-            </div>
-            <div className={`content ${!isActive ? 'content-open' : ''}`}>{content}</div>
+            <div className={`content ${isActive === index ? 'content-open' : ''}`}>{item.content}</div>
          </div>
-
-         {/* Om knapp trycks på ska den delen öppnas.
-         Klickas knappen igen ska den stängas.
-         Om man klickar på en annan knapp ska föregående stängas. */}
-
-
-
-
-
-
-
-
-
-
-
-
-
-        {/* <div className="list-item">
-            <input type="radio" name="radio" id="radio2" />
-            <div className="title">
-                <label htmlFor="radio2" className="question">What formats can I download my transaction history in?</label>
-                <label htmlFor="radio2" className="btn btn-circle"><i className="fa-solid fa-chevron-down"></i></label>
-            </div>
-            <div className="content">Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.</div>
-        </div>
-        <div className="list-item">
-            <input type="radio" name="radio" id="radio3" />
-            <div className="title">
-                <label htmlFor="radio3" className="question">Can I schedule future transfers?</label>
-                <label htmlFor="radio3" className="btn btn-circle"><i className="fa-solid fa-chevron-down"></i></label>
-            </div>
-            <div className="content">Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.</div>
-        </div>
-        <div className="list-item">
-            <input type="radio" name="radio" id="radio4" />
-            <div className="title">
-                <label htmlFor="radio4" className="question">When can I use Banking App services?</label>
-                <label htmlFor="radio4" className="btn btn-circle"><i className="fa-solid fa-chevron-down"></i></label>
-            </div>
-            <div className="content">Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat. <br /><br />Ornare senectus fusce dignissim ut. Integer
-                consequat in eu tortor, faucibus et lacinia posuere. Turpis sit viverra lorem suspendisse lacus aliquam auctor vulputate. Quis egestas aliquam nunc purus lacus, elit leo elit facilisi. Dignissim amet adipiscing massa integer.</div>
-        </div>
-        <div className="list-item">
-            <input type="radio" name="radio" id="radio5" />
-            <div className="title">
-                <label htmlFor="radio5" className="question">Can I create my own password that is easy for me to remember?</label>
-                <label htmlFor="radio5" className="btn btn-circle"><i className="fa-solid fa-chevron-down"></i></label>
-            </div>
-            <div className="content">Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.</div>
-        </div>
-        <div className="list-item">
-            <input type="radio" name="radio" id="radio6" />
-            <div className="title">
-                <label htmlFor="radio6" className="question">What happens if I forget or lose my password?</label>
-                <label htmlFor="radio6" className="btn btn-circle"><i className="fa-solid fa-chevron-down"></i></label>
-            </div>
-            <div className="content">Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.</div>
-        </div> */}
+        ))}
+       
     </>
   )
 }
