4074e73bfdc04300aed5a544a0ccec868d98e8e3
Åsa Johansson <asa.j.1989@gmail.com>
2024-11-07 19:24:11 +0100

    validering av kontaktformulär

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,166 @@
+import React, { useEffect, useState } from 'react'
+
+const ContactForm = () => {
+
+
+    const [formData, setFormData] = useState({ fullName: '', email: '', specialist: '' });
+    const [errors, setErrors] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({...formData, [name]: value});
+
+        if(value.trim() === '') {
+            setErrors(prevErrors => ({...prevErrors, [name]: 'This field is required!'}))
+        }
+        else {
+            setErrors(prevErrors => ({...prevErrors, [name]: ''}))
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const newErrors = {};
+
+        Object.keys(formData).forEach((input) => {
+            if(formData[input].trim() === '') {
+                newErrors[input] = 'This field is required!';
+            }
+        })
+
+        if(Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
+            return
+        }
+
+        
+        try {
+            const res = await fetch('https://win24-assignment.azurewebsites.net/api/forms/contact', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });  
+            
+            if(res.ok) {
+                setIsSubmitted(true);
+                setFormData({ fullName: '', email: '', specialist: '' });
+                console.log('The request was sent successfully.');
+            } else {
+                const error = await res.json();
+                console.log(error.errors)
+                setErrors(error.errors)
+                // console.log(error)
+            }
+        }
+        catch(error) {
+            console.log('There was an error!', error);
+        }
+    }
+
+    useEffect(() => {
+        if(isSubmitted) {
+            const timer = setTimeout(() => {
+                setIsSubmitted(false);
+            }, 5000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [isSubmitted]);
+
+    if(isSubmitted) {
+        return (
+            <div className="box">
+                <h3>Thank you for contacting us!</h3>
+                <p>We will get back to you within 2-3 days.</p>
+            </div>
+        )
+    }
+
+    const specialist = [
+        {
+            id: 1,
+            title: 'Cardiology'
+        },
+        {
+            id: 2,
+            title: 'Dermatology'
+        },
+        {
+            id: 3,
+            title: 'Endoskopy'
+        },
+        {
+            id: 4,
+            title: 'Neurology'
+        },
+        {
+            id: 5,
+            title: 'Obstetrics/Gynecology'
+        },
+        {
+            id: 6,
+            title: 'Orthopedy'
+        },
+        {
+            id: 7,
+            title: 'Palliative team'
+        },
+        {
+            id: 8,
+            title: 'Pediatric medicine'
+        },
+        {
+            id: 9,
+            title: 'Psychiatry'
+        },
+        {
+            id: 10,
+            title: 'Radiology'
+        },
+        {
+            id: 11,
+            title: 'Rheumatology'
+        },
+        {
+            id: 12,
+            title: 'Urology'
+        },
+    ]
+
+  return (
+    <div className="form-container">
+        <h2>Get Online Consultation</h2>        
+        <form className="form" onSubmit={handleSubmit} noValidate>
+            <div className="input-group">
+                <label htmlFor="">Full name</label>
+                <input value={formData.fullName} onChange={handleChange} name="fullName" className={`form-input ${errors.fullName ? 'validate-error' : ''}`} type="text" placeholder="Enter full name" />
+                <span className="msg-error">{errors.fullName && errors.fullName || errors.FullName && errors.FullName}</span>
+            </div>
+            <div className="input-group">
+                <label htmlFor="">Email address</label>
+                <input value={formData.email} onChange={handleChange} name="email" className={`form-input ${errors.email ? 'validate-error' : ''}`} type="email" placeholder="Enter email address" />
+                <span className="msg-error">{errors.email && errors.email || errors.Email && errors.Email}</span>
+
+            </div>
+            <div className="input-group">
+                <label htmlFor="">Specialist</label>
+                <input value={formData.specialist} onChange={handleChange} name="specialist" className={`form-input ${errors.specialist ? 'validate-error' : ''}`} list="specialist" />
+                <span className="msg-error">{errors.specialist && errors.specialist || errors.Specialist && errors.Specialist}</span>
+
+                <datalist id="specialist">
+                {specialist.map((item) => (
+                    <option key={item.id}>{item.title}</option>
+                ))}
+                </datalist>
+            </div>
+            <button className="btn btn-primary">Make an appointment</button>
+        </form>
+    </div>
+  )
+}
+
+export default ContactForm
--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,47 +1,69 @@
 import React, { useState, useEffect } from 'react'
 
-// import ConfirmForm from './ConfirmForm';
-
 import Notification from '../images/notification.svg'
-import { Link } from 'react-router-dom';
-
 
 
 const Subscribe = () => {
 
-  const [email, setEmail] = useState('');
-  const [isValidated, setIsValidated] = useState(true);
+  const [formData, setFormData] = useState({ email: '' });
   const [isSubmitted, setIsSubmitted] = useState(false);
-  // const [isConfirmed, setIsConfirmed] = useState(false);
+  const [isValidated, setIsValidated] = useState(true);
 
-  const handleSubmit = (e) => {
+  const handleChange = (e) => {
+    setIsValidated(true);
+    const { name, value } = e.target;
+    setFormData({...formData, [name]: value});
+  }
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if(email.trim() === '') {
+    const res = await fetch('https://win24-assignment.azurewebsites.net/api/forms/subscribe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    });
+
+    if(res.ok) {
+      setIsSubmitted(true);
+      setFormData({ email: '' });
+    }
+    else {
       setIsValidated(false);
-      return
+      setFormData({ email: '' });
+      
+      const error = res.json();
+      console.log(error);
     }
 
-    setIsValidated(true);
-    setIsSubmitted(true);
-    // setIsConfirmed(true);
-
   }
+
 
   useEffect(() => {
     if(isSubmitted) {
 
       const timer = setTimeout(() => {
         setIsSubmitted(false);
-      }, 8000);
+      }, 5000);
 
-      return () => clearTimeout(timer);
+      return () => clearTimeout(timer)
     }
   }, [isSubmitted]);
 
   
-  
-
+  if(isSubmitted) {
+    return (
+      <section id="subscribe">
+        <div className="container">
+          <div className="box">
+            <h3>Thank you for subscribing!</h3>
+          </div>
+        </div>
+      </section>
+    )
+  }
 
   return (
     <>
@@ -52,21 +74,11 @@ const Subscribe = () => {
               <h3 id="text">Subscribe to our newsletter</h3>
               <h3 className="text-desktop">Subscribe to our newsletter to stay informed about latest updates</h3>
             </div>
-
-            {isSubmitted && (
-              <div className="box">
-                <h3>Thank's for subscribing!</h3>
-              </div>
-            )}
-
-            {!isSubmitted && (
-              <form className="form" onSubmit={handleSubmit}>
-                  <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" className={`form-input ${isValidated ? '' : 'validate-error'}`} name="email" placeholder="Enter your email" />
-                  {!isValidated && <label className="msg-error">Please enter a valid email address.</label>}
-                <button className="btn btn-primary" htmlFor="email">Subscribe</button>
-              </form>
-              )}
-
+            <form className="form" onSubmit={handleSubmit} noValidate>
+              <input value={formData.email} onChange={handleChange} type="email" name="email" className={`form-input ${isValidated ? '' : 'validate-error'}`} placeholder="Enter your email" />
+              {!isValidated && <label className="msg-error">Please enter a valid email address.</label>}
+              <button className="btn btn-primary" htmlFor="email">Subscribe</button>
+            </form>
           </div>
       </section>
     </>
