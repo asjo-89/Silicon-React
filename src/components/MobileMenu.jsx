c106fb002c798778ed370d941399af3da22f43ea
Åsa Johansson <asa.j.1989@gmail.com>
2024-11-15 11:41:12 +0100

    mobilmeny stängs när man klickar utanför den

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useTheme } from '../contexts/ThemeContext'
 
@@ -7,13 +7,13 @@ import MenuDark from '../images/menuDark.svg'
 
 const MobileMenu = () => {
     const { theme } = useTheme();
-    const [isActive, setIsActive] = useState(false)
+    const [isActive, setIsActive] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef(null);
 
     const toggleActive = () => {
         setIsActive(!isActive);
     }
-
-    const [isOpen, setIsOpen] = useState(false);
 
     const handleClick = () => {
         setIsOpen(!isOpen);
@@ -24,19 +24,35 @@ const MobileMenu = () => {
         setIsOpen(false);
     }
 
+    const closeOnOutsideClick = (e) => {
+        if(menuRef.current && !menuRef.current.contains(e.target)) {
+            setIsOpen(false);
+        }
+    } 
+
+    useEffect(() => {
+        document.addEventListener('click', closeOnOutsideClick);
+
+        return () => {
+            document.removeEventListener('click', closeOnOutsideClick);
+        }
+    }, [])
+
     return (
         <>
-            <button className={`btn-mobile-menu ${isActive ? 'menu-active' : ''}`} aria-label="Menu" onClick={handleClick}>
-                <img id="menu-bars" src={theme === 'light-theme' ? MenuLight : MenuDark} alt="Menu" />
-            </button>
-            <div className="dropdown-container">
-                {isOpen && (
-                    <div className="dropdown-menu">
-                        <Link className="link" to="/" onClick={closeDropDown}><p>Home</p></Link>
-                        <a className="link" href="/#app-features" onClick={closeDropDown}><p>Features</p></a>
-                        <Link className="link" to="/contact" onClick={closeDropDown}><p>Contact</p></Link>
-                    </div>
-                )}
+            <div ref={menuRef}>
+                <button className={`btn-mobile-menu ${isActive ? 'menu-active' : ''}`} aria-label={isOpen ? 'Close menu' : 'Open menu'}  onClick={handleClick}>
+                    <img id="menu-bars" src={theme === 'light-theme' ? MenuLight : MenuDark} alt="Menu" />
+                </button>
+                <div className="dropdown-container">
+                    {isOpen && (
+                        <div className="dropdown-menu">
+                            <Link className="link" to="/" onClick={closeDropDown}><p>Home</p></Link>
+                            <a className="link" href="/#app-features" onClick={closeDropDown}><p>Features</p></a>
+                            <Link className="link" to="/contact" onClick={closeDropDown}><p>Contact</p></Link>
+                        </div>
+                    )}
+                </div>
             </div>
         </>
     )
